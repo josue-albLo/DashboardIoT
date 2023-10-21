8c3449753fc468d3c9be26298aca8d4b229e3f93
Josue Guevara <guevarajosue637@gmail.com>
2023-10-21 12:05:42 -0600

    fix path

--- src/api/getData.js
+++ src/api/getData.js
@@ -1,7 +1,7 @@
 
 export const getDataSeverTwo = async () => {
     try {
-        const response = await fetch('http://www.iotapi2023.somee.com/api/Usuario/listarEvento')
+        const response = await fetch('https://fastapi-production-a16f.up.railway.app/allEvents')
         return response.json();
     } catch (err){
         console.log(err);
